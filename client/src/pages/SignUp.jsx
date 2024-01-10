54ac56b140e545afbc81829e055ce15b04e3b456
Nihal Ahmed Latif <nihal.latif@gmail.com>
2024-01-11 01:28:39 +0530

    sign up page functionality

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,16 +1,55 @@
+import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 export default function SignUp() {
+  const [formData, setFormData] = useState({});
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value,
+    });
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      setLoading(true);
+      const res = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.success === false) {
+        setLoading(false);
+        setError(data.message);
+        return;
+      }
+      setLoading(false);
+      setError(null);
+      navigate("/sign-in");
+    } catch (error) {
+      setLoading(false);
+      setError(error.message);
+    }
+  };
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
       <div className="bg-white p-7 rounded-2xl">
-        <form className="flex flex-col gap-4">
+        <form onSubmit={handleSubmit} className="flex flex-col gap-4">
           <input
             type="text"
             placeholder="Username"
             className="border p-3 rounded-lg"
             id="username"
+            onChange={handleChange}
           />
 
           <input
@@ -18,6 +57,7 @@ export default function SignUp() {
             placeholder="Email"
             className="border p-3 rounded-lg"
             id="email"
+            onChange={handleChange}
           />
 
           <input
@@ -25,10 +65,14 @@ export default function SignUp() {
             placeholder="Password"
             className="border p-3 rounded-lg"
             id="password"
+            onChange={handleChange}
           />
 
-          <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
-            Sign Up
+          <button disabled={loading} className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+            {!loading ? "Sign Up" : <svg class="animate-spin mx-auto h-5 w-5 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+      <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+      <path className="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+    </svg>}
           </button>
         </form>
         <div className="flex justify-center gap-2 mt-5">
