f99f5912c005ced700cc85286a830e85eaa6f552
Nihal Ahmed Latif <nihal.latif@gmail.com>
2024-01-10 14:02:53 +0530

    installed react using vite and tailwind for css

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div>
+      <h1 className="text-green-500">Test</h1>
+      <div className="table">
+        hello
+      </div>
+    </div>
+  );
+}
