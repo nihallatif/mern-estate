4b0a4dee16d6404cacf49d2055f5b46cd249a2f6
Nihal Ahmed Latif <nihal.latif@gmail.com>
2024-01-11 00:38:27 +0530

    sign up page ui

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -7,7 +7,7 @@ export default function Header() {
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/">
           <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-            <span className="text-purple-500">InfraEstate</span>
+            <span className="text-slate-700">InfraEstate</span>
           </h1>
         </Link>
         <form className="bg-slate-100 p-3 rounded-lg flex items-center">
--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SignUp() {
   return (
-    <div>Sign Up</div>
-  )
+    <div className="p-3 max-w-lg mx-auto">
+      <h1 className="text-3xl text-center font-semibold my-7">Sign Up</h1>
+      <div className="bg-white p-7 rounded-2xl">
+        <form className="flex flex-col gap-4">
+          <input
+            type="text"
+            placeholder="Username"
+            className="border p-3 rounded-lg"
+            id="username"
+          />
+
+          <input
+            type="email"
+            placeholder="Email"
+            className="border p-3 rounded-lg"
+            id="email"
+          />
+
+          <input
+            type="password"
+            placeholder="Password"
+            className="border p-3 rounded-lg"
+            id="password"
+          />
+
+          <button className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80">
+            Sign Up
+          </button>
+        </form>
+        <div className="flex justify-center gap-2 mt-5">
+          <p>Have an account?</p>
+          <Link to={"/sign-in"}>
+            <span className="text-slate-900 hover:underline">Sign in</span>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
 }
