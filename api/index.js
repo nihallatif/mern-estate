458bece9cb5e42660308b41b864e53bf0ffe3492
Nihal Ahmed Latif <nihal.latif@gmail.com>
2024-01-11 23:50:47 +0530

    create update user api

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import authRouter from "./routes/auth.route.js";
+import userRouter from "./routes/user.route.js";
+import cookieParser from 'cookie-parser';
 dotenv.config();
 
 mongoose.connect(process.env.MONGO).then(() => {
@@ -15,12 +17,15 @@ const app = express();
 
 app.use(express.json());
 
+app.use(cookieParser());
+
 app.listen(3000, () => {
   console.log("Server is running on port 3000!");
 });
 
 //API Routes
 app.use("/api/auth", authRouter);
+app.use('/api/user', userRouter);
 
 //Error handling
 app.use((err, req, res, next) => {
